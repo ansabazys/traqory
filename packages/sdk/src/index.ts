2d2f438081018946f236914a5aed8b4b41412b04
ansabazys <ansabazys@gmail.com>
2026-06-03 16:02:26 +0530

    feat(sdk): implement event tracking

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,6 +1,7 @@
 export { getVisitorId } from './core/visitor';
 export { getSessionId } from './core/session';
 export { identify } from "./features/identify";
+export { track } from "./features/track";
 
 import { client } from './client';
 import type { SDKConfig } from './types';
